9232919f0743b8df07bd047643d0dfae3b345182
ibrahimi46 <anasibrahimi4664@gmail.com>
2025-10-20 01:01:46 +0300

    made protected adding of products for admins only

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,12 +2,29 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db"
 import { products as productsTable}  from "@/db/schema";
 import { eq } from "drizzle-orm";
+import jwt from "jsonwebtoken"
 
 
 export async function POST(req:NextRequest) {
     try {
-        const body = await req.json();
-        const {name, description, category, price} = body;
+
+        const authHeader = req.headers.get("authorization");
+        if (!authHeader) return NextResponse.json({error: "Missing token"}, {status: 404})
+        
+        const token = authHeader.split(" ")[1];
+
+        let decoded;
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET!) as {userId: number, isAdmin: boolean};
+        } catch(error) {
+            console.error(error)
+            return NextResponse.json({error: "Invalid token"}, {status: 401})
+        }
+
+        if (!decoded.isAdmin) {
+            return NextResponse.json({error: "Unauthorised to add product"}, {status: 401})
+        } else {
+            const {name, description, category, price} = await req.json();
 
         if (!name || !category || !price) {
             return NextResponse.json({error: "Missing required fields"}, {status: 400})
@@ -21,6 +38,9 @@ export async function POST(req:NextRequest) {
         })
 
         return NextResponse.json({success: true, message: "Product added!"})
+        }
+
+        
     }
     catch(error) {
         console.error(error)
