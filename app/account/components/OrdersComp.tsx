256899a5542c1d0f9c0f106d3fa6d8b65099e9df
ibrahimi46 <anasibrahimi4664@gmail.com>
2025-10-31 00:14:28 +0300

    Implemented the filter in orders page for user

--- app/account/components/OrdersComp.tsx
+++ app/account/components/OrdersComp.tsx
@@ -1,5 +1,5 @@
 import assets from "@/assets";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Image from "next/image";
 import BestSeller from "@/app/components/home-components/BestSeller";
 import NoDataPlaceholder from "./NoDataPlaceholder";
@@ -10,8 +10,8 @@ import { dateFormatter } from "@/app/utils/dateFormatter";
 interface OrderItemProps {
   status: string;
   createdAt: string;
-  totalAmount: string;
-  itemCount: string;
+  totalAmount: number;
+  itemCount: number;
 }
 
 const OrderItem = ({
@@ -96,8 +96,14 @@ const OrderItem = ({
 };
 
 const OrdersComp = () => {
+  const [filter, setFilter] = useState<string>("all");
   const context = useContext(UserDataContext);
   const { orders } = context!;
+
+  const filteredOrders = orders.filter((order) => {
+    if (filter === "all") return true;
+    return order.status === filter;
+  });
 
   return (
     <div>
@@ -105,20 +111,49 @@ const OrdersComp = () => {
         <div className="flex flex-col gap-4 mt-2">
           <h1 className="hidden md:block font-bold">My Orders</h1>
           <div className="flex gap-2  text-body-sm">
-            <div className="bg-black-100 px-6 py-2 rounded-2xl border border-black-200">
+            <div
+              className={`bg-black-100 px-6 py-2 rounded-2xl border transition-all duration-300 border-black-20 cursor-pointer
+                ${filter === "all" && "bg-primary-600 text-white font-semibold"}
+                `}
+              onClick={() => setFilter("all")}
+            >
               <p>All</p>
             </div>
-            <div className="bg-black-100 px-6 py-2 rounded-2xl border border-black-200">
+            <div
+              className={`bg-black-100 px-6 py-2 rounded-2xl border transition-all duration-300 border-black-20 cursor-pointer
+                ${
+                  filter === "in_progress" &&
+                  "bg-primary-600 text-white font-semibold"
+                }
+                `}
+              onClick={() => setFilter("in_progress")}
+            >
               <p>In Progress</p>
             </div>
-            <div className="bg-black-100 px-6 py-2 rounded-2xl border border-black-200">
+            <div
+              className={`bg-black-100 px-6 py-2 rounded-2xl border transition-all duration-300 border-black-20 cursor-pointer
+                ${
+                  filter === "delivered" &&
+                  "bg-primary-600 text-white font-semibold"
+                }
+                `}
+              onClick={() => setFilter("delivered")}
+            >
               <p>Delivered</p>
             </div>
-            <div className="bg-black-100 px-6 py-2 rounded-2xl border border-black-200">
+            <div
+              className={`bg-black-100 px-6 py-2 rounded-2xl border transition-all duration-300 border-black-20 cursor-pointer
+                ${
+                  filter === "cancelled" &&
+                  "bg-primary-600 text-white font-semibold"
+                }
+                `}
+              onClick={() => setFilter("cancelled")}
+            >
               <p>Cancelled</p>
             </div>
           </div>
-          {orders.map((order) => {
+          {filteredOrders.map((order) => {
             return (
               <OrderItem
                 key={order.id}
