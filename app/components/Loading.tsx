5d299081008115d644a1dce640a482d3656b4e57
ibrahimi46 <anasibrahimi4664@gmail.com>
2025-11-01 17:11:53 +0300

    Style: Added logo in loading component

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,9 +1,18 @@
+import assets from "@/assets";
+import Image from "next/image";
+
 const Loading = () => {
   return (
     <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/50 backdrop-blur-sm pointer-events-auto">
-      <div className="flex flex-col items-center gap-3 bg-white px-10 py-5 rounded-3xl">
-        <div className="flex items-center"></div>
-        <div className="w-12 h-12 border-4 border-t-primary-600 border-b-primary-600 border-l-gray-300 border-r-gray-300 rounded-full animate-spin"></div>
+      <div className="flex flex-col relative items-center gap-3 px-8 py-8 rounded-3xl">
+        <div className="w-16 h-16 border-4 border-t-primary-600 border-b-primary-600 border-l-gray-300 border-r-gray-300 rounded-full animate-spin"></div>
+        <Image
+          src={assets.logo}
+          height={40}
+          width={40}
+          alt="logo"
+          className="absolute top-11 flex items-center"
+        />
       </div>
     </div>
   );
