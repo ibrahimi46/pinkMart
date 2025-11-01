33582c2349cd9cfedbc73acf4f9aac8bd31c4b0b
ibrahimi46 <anasibrahimi4664@gmail.com>
2025-11-01 12:15:29 +0300

    Fixed: loading component in bestseller main page was inconsistent

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import assets from "@/assets";
-
 const Loading = () => {
   return (
-    <div className="h-96 flex items-center justify-center">
+    <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/50 backdrop-blur-sm pointer-events-auto">
       <div className="flex flex-col items-center gap-3 bg-white px-10 py-5 rounded-3xl">
         <div className="flex items-center"></div>
         <div className="w-12 h-12 border-4 border-t-primary-600 border-b-primary-600 border-l-gray-300 border-r-gray-300 rounded-full animate-spin"></div>
